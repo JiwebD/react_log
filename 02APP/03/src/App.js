d629c3e3c9ca5c9a93e7f1248b97e3615b3ac74f
JwebD <gkdlfnzo12@naver.com>
2025-05-22 17:55:48 +0900

    2025-05-21 01 리액트 init 2025-05-22 02 리액트 02~05

---
+++ 02APP/03/src/App.js
@@ -0,0 +1,16 @@
+import MyComponent from "./02COMPONENT/MyComponent";
+
+function App() {
+
+  return (
+    <>
+
+      <div className="App">
+        <MyComponent />
+      </div>
+
+    </>
+  );
+}
+
+export default App;
